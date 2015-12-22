f450404049828e7146a8a1370819e3fd903a9c71
JM <JM@192.168.250.45>
2015-12-22 20:10:43 +0700

    Fix bug

--- SSF/KanomChanCore/src/org/kanomchan/core/common/constant/CheckService.java
+++ SSF/KanomChanCore/src/org/kanomchan/core/common/constant/CheckService.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 package org.kanomchan.core.common.constant;
 
 import org.kanomchan.core.common.exception.NonRollBackException;
@@ -12,17 +10,3 @@ public interface CheckService {
 	public boolean checkClearableList(String tableName, String columnName)throws RollBackException, NonRollBackException;
 	public boolean checkIncludeMinusOne(String name, String columnName)throws RollBackException, NonRollBackException;
 }
-=======
-package org.kanomchan.core.common.constant;
-
-import org.kanomchan.core.common.exception.NonRollBackException;
-import org.kanomchan.core.common.exception.RollBackException;
-
-public interface CheckService {
-	public boolean checkNeedleList(String tableName, String columnName)throws RollBackException, NonRollBackException;
-	public boolean checkTableLang(String tableName)throws RollBackException, NonRollBackException;
-	public boolean checkColumnNameInTableLang(String tableName, String columnName)throws RollBackException, NonRollBackException;
-	public boolean checkClearableList(String tableName, String columnName)throws RollBackException, NonRollBackException;
-	public boolean checkIncludeMinusOne(String name, String columnName)throws RollBackException, NonRollBackException;
-}
->>>>>>> branch 'master' of https://github.com/viatoro/kanomchan.git
