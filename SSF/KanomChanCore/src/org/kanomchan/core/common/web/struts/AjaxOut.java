98014afaa96b56ac2421ef270589973145f8cb13
kwan <jarupong.wiboonchart@gmail.com>
2015-12-28 15:33:34 +0700

    Fix Target

--- SSF/KanomChanCore/src/org/kanomchan/core/common/web/struts/AjaxOut.java
+++ SSF/KanomChanCore/src/org/kanomchan/core/common/web/struts/AjaxOut.java
@@ -1,13 +1,12 @@
 package org.kanomchan.core.common.web.struts;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({METHOD, FIELD})
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({METHOD})
 @Retention(RUNTIME)
 public @interface AjaxOut {
 
