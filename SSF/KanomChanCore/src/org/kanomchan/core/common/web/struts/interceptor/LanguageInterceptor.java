bc2dd374e0dd54fcb691a7a2dddc4b6acbe77f3c
beer <jshowz@gmail.com>
2016-11-11 18:47:22 +0700

    Change process

--- SSF/KanomChanCore/src/org/kanomchan/core/common/web/struts/interceptor/LanguageInterceptor.java
+++ SSF/KanomChanCore/src/org/kanomchan/core/common/web/struts/interceptor/LanguageInterceptor.java
@@ -1,11 +1,16 @@
 package org.kanomchan.core.common.web.struts.interceptor;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts2.ServletActionContext;
 import org.kanomchan.core.common.constant.CommonConstant;
+import org.kanomchan.core.common.context.ApplicationContextUtil;
 import org.kanomchan.core.common.context.CurrentThread;
 import org.kanomchan.core.common.processhandler.ProcessContext;
 import org.kanomchan.core.common.service.ConfigService;
@@ -31,23 +36,24 @@ public class LanguageInterceptor  extends AbstractInterceptor{
 	
 	
 	@Override
-	public String intercept(ActionInvocation invocation) throws Exception {
-		ActionContext context = invocation.getInvocationContext();
-		ProcessContext processContext = CurrentThread.getProcessContext();
-		Map<String, Object> sessions = context.getSession();
-		HttpServletRequest request = ServletActionContext.getRequest();
-		String langKey = request.getParameter(CommonConstant.PARAMETER.LANG_KEY);
-		if(langKey==null||langKey.equals("")){
-			langKey = (String) sessions.get(CommonConstant.SESSION.LANG_KEY)+"";
-			if(langKey==null||langKey.equals("")||langKey.equalsIgnoreCase("null")){
-				langKey = configService.get(CommonConstant.DEFAULT_LANG_CONFIG);
-				langKey = langKey==null||"".equals(langKey)?CommonConstant.DEFAULT_LANG_KEY:langKey;
-			}
-		}
-		processContext.setLang(langKey);
-		sessions.put(CommonConstant.SESSION.LANG_KEY,langKey);
-		String s = invocation.invoke();
-			
-		return s;
-	}
+	 public String intercept(ActionInvocation invocation) throws Exception {
+	  ActionContext context = invocation.getInvocationContext();
+	  ProcessContext processContext = CurrentThread.getProcessContext();
+	  Map<String, Object> sessions = context.getSession();
+	//  HttpServletRequest request = ServletActionContext.getRequest();
+	//  String langKey = request.getParameter(CommonConstant.PARAMETER.LANG_KEY);
+	//  if(langKey==null||langKey.equals("")){
+	//   langKey = (String) sessions.get(CommonConstant.SESSION.LANG_KEY)+"";
+	//   if(langKey==null||langKey.equals("")||langKey.equalsIgnoreCase("null")){
+//	    langKey = configService.get(CommonConstant.DEFAULT_LANG_CONFIG);
+//	    langKey = langKey==null||"".equals(langKey)?CommonConstant.DEFAULT_LANG_KEY:langKey;
+	//   }
+	//  }
+	  Locale nativeLocale = ActionContext.getContext().getLocale();
+	  processContext.setLang(nativeLocale.getISO3Language());
+	  sessions.put(CommonConstant.SESSION.LANG_KEY,nativeLocale.getISO3Language());
+	  String s = invocation.invoke();
+	   
+	  return s;
+	 }
 }
